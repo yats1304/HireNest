39d96ae6a0f9d9d83ac08b5d7db035d265c4a8dc
yats1304 <yats1304@users.noreply.github.com>
2026-06-02 23:06:33 +0530

    feat: reset passowrd

--- services/auth/src/controller/controller.auth.ts
+++ services/auth/src/controller/controller.auth.ts
@@ -150,3 +150,44 @@ export const forgotPassword = TryCatch(async(req,res,next)=>{
         message: "If that email exists, we have sent a reset link",
     })
 })
+
+export const resetPassword = TryCatch(async(req,res,next)=>{
+    const {token} = req.params;
+    const {password} = req.body;
+
+    let decoded: any;
+
+    try {
+        decoded = jwt.verify(token as string, process.env.JWT_SECRET as string)
+    } catch (error) {
+        throw new ErrorHandler(400, "Expired token")
+    }
+
+    if(decoded.type !== "reset"){
+        throw new ErrorHandler(400, "Invalid token type")
+    }
+
+    const email = decoded.email
+
+    const storedToken = await redisClient.get(`forgot:${email}`)
+
+    if(!storedToken || storedToken !== token){
+        throw new ErrorHandler(400,"Token has been expired")
+    }
+
+    const users = await sql`SELECT user_id FROM users WHERE email = ${email}`
+
+    if(users.length === 0){
+        throw new ErrorHandler(404, "User not found!")
+    }
+
+    const user = users[0]
+
+    const hashedPassword = await bcrypt.hash(password, 10)
+
+    await sql`UPDATE users SET password = ${hashedPassword} WHERE user_id = ${user.user_id}`
+
+    await redisClient.del(`forgot:${email}`)
+
+    res.json({ message: "Password changes successfully!"})
+})
--- services/auth/src/routes/route.auth.ts
+++ services/auth/src/routes/route.auth.ts
@@ -1,5 +1,5 @@
 import express from "express"
-import { forgotPassword, loginUser, registerUser } from "../controller/controller.auth.js";
+import { forgotPassword, loginUser, registerUser, resetPassword } from "../controller/controller.auth.js";
 import uploadFile from "../middleware/multer.middleware.js";
 
 const router = express.Router();
@@ -7,5 +7,6 @@ const router = express.Router();
 router.post("/register",uploadFile, registerUser)
 router.post("/login", loginUser)
 router.post("/forgot", forgotPassword)
+router.post("/reset/:token", resetPassword)
 
 export default router
