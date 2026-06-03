50855d23655ee5559d5f5e385e61bb34f112bb37
yats1304 <yats1304@users.noreply.github.com>
2026-06-03 16:40:06 +0530

    feat: add and delete skill route

--- services/user/src/controller/user.controller.ts
+++ services/user/src/controller/user.controller.ts
@@ -132,3 +132,77 @@ export const updateResume = TryCatch(async(req:AuthenticatedRequest, res,next)=>
         updatedUser
     })
 })
+
+export const addSkillsToUser = TryCatch(async(req:AuthenticatedRequest, res)=>{
+    const userId = req.user?.user_id
+    const {skillName} = req.body;
+
+    if(!skillName || skillName.trim()===0){
+        throw new ErrorHandler(400, "Please provide a skill name");
+    }
+
+    let wasSkillAdded = false;
+
+    try {
+        await sql`BEGIN`
+
+        const users = await sql`SELECT user_id FROM users WHERE user_id = ${userId}`
+
+        if(users.length === 0){
+            throw new ErrorHandler(404, "User not found!")
+        }
+
+        const [skill] = await sql`INSERT INTO skills (name) VALUES (${skillName.trim()}) 
+        ON CONFLICT (name) DO UPDATE SET name = EXCLUDED.name RETURNING skill_id`;
+
+        const skillId = skill.skill_id;
+
+        const insertionResult = await sql`INSERT INTO user_skills (user_id, skill_id) VALUES (${userId}, ${skillId})
+        ON CONFLICT (user_id, skill_id) DO NOTHING RETURNING user_id`;
+
+        if(insertionResult.length > 0){
+            wasSkillAdded = true
+        }
+
+        await sql`COMMIT`;
+    } catch (error) {
+        await sql`ROLLBACK`;
+        throw error
+    }
+
+    if(!wasSkillAdded){
+        return res.status(200).json({
+            message: "User Already possesses this skill"
+        })
+    }
+
+    res.json({
+        message: `Skill ${skillName.trim()} is added successfully!`
+    })
+})
+
+export const deleteSkillFromUser = TryCatch(async(req:AuthenticatedRequest, res)=>{
+    const user = req.user
+
+     if(!user){
+        throw new ErrorHandler(401, "Authentication required!")
+    }
+
+    const {skillName} = req.body;
+
+    if(!skillName || skillName.trim()===0){
+        throw new ErrorHandler(400, "Please provide a skill name");
+    }
+
+    const result = await sql`DELETE FROM user_skills WHERE user_id = ${user.user_id} AND skill_id = (SELECT  skill_id FROM skills 
+    WHERE name = ${skillName.trim()}) RETURNING user_id;`;
+
+    if(result.length===0){
+        throw new ErrorHandler(404, `Skill ${skillName.trim()} was not found`)
+    }
+
+    res.json({
+        message : `Skill ${skillName.trim()} was deleted successfully!`
+    })
+
+})
--- services/user/src/routes/user.route.ts
+++ services/user/src/routes/user.route.ts
@@ -1,6 +1,6 @@
 import express from "express"
 import { isAuth } from "../middleware/auth.middleware.js"
-import { getUserProfile, myProfile, updateProfilePicture, updateResume, updateUserProfile } from "../controller/user.controller.js"
+import { addSkillsToUser, deleteSkillFromUser, getUserProfile, myProfile, updateProfilePicture, updateResume, updateUserProfile } from "../controller/user.controller.js"
 import uploadFile from "../middleware/multer.middleware.js"
 
 const router = express.Router()
@@ -10,5 +10,7 @@ router.get("/:userId", isAuth, getUserProfile)
 router.put("/update/profile", isAuth, updateUserProfile)
 router.put("/update/pic", isAuth, uploadFile, updateProfilePicture)
 router.put("/update/resume", isAuth, uploadFile, updateResume)
+router.post("/skill/add", isAuth, addSkillsToUser)
+router.delete("/skill/delete", isAuth, deleteSkillFromUser)
 
 export default router
