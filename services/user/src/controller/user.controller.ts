acdf9831b0e089a0e3da5257db984ef53611e61d
yats1304 <yats1304@users.noreply.github.com>
2026-06-06 21:52:11 +0530

    feat: apply job and get all job apply by user route created

--- services/user/src/controller/user.controller.ts
+++ services/user/src/controller/user.controller.ts
@@ -206,3 +206,71 @@ export const deleteSkillFromUser = TryCatch(async(req:AuthenticatedRequest, res)
     })
 
 })
+
+export const applyForJob = TryCatch(async(req:AuthenticatedRequest, res)=>{
+    const user = req.user
+
+    if(!user){
+        throw new ErrorHandler(401, "Authentication required!")
+    }
+
+    if(user.role !== "jobseeker"){
+        throw new ErrorHandler(403, "Forbidden: You are not allowed for this API")
+    }
+
+    const applicant_id = user.user_id;
+
+    const resume = user.resume
+
+    if(!resume){
+        throw new ErrorHandler(400, "You need to add resume in your profile to apply for this job")
+    }
+
+    const {job_id} = req.body;
+
+    if(!job_id){
+        throw new ErrorHandler(400, "job id is required!")
+    }
+
+    const [job] = await sql`SELECT is_active FROM jobs WHERE job_id =${job_id}`
+
+    if(!job){
+        throw new ErrorHandler(404, "No jobs with this Id")
+    }
+
+    if(!job.is_active){
+        throw new ErrorHandler(400, "Job is not active")
+    }
+
+    const now = Date.now()
+
+    const subTime = req.user?.subscription ? new Date(req.user.subscription).getTime() : 0;
+
+    const isSubscribed = subTime > now;
+
+    let newApplication;
+
+    try {
+        [newApplication] = await sql`INSERT INTO applications (job_id, applicant_id, applicant_email,
+        resume, subscribed) VALUES (${job_id}, ${applicant_id}, ${user?.email}, ${resume}, ${isSubscribed})`
+    } catch (error: any) {
+        if(error.code === "23505"){
+            throw new ErrorHandler(409, "You have already applied to this job!")
+        }
+        throw error;
+    }
+
+    res.json({
+        message: "Applied fot job successfully!",
+        application: newApplication
+    })
+})
+
+export const getAllApplications = TryCatch(async(req:AuthenticatedRequest, res)=>{
+    const applications = await sql `
+        SELECT * , j.title AS job_title, j.salary AS job_salary, j.location AS job_location
+        FROM applications a JOIN jobs j ON a.job_id = j.job_id WHERE a.applicant_id  = ${req.user?.user_id}
+    `;
+
+    res.json(applications)
+})
--- services/user/src/routes/user.route.ts
+++ services/user/src/routes/user.route.ts
@@ -1,6 +1,6 @@
 import express from "express"
 import { isAuth } from "../middleware/auth.middleware.js"
-import { addSkillsToUser, deleteSkillFromUser, getUserProfile, myProfile, updateProfilePicture, updateResume, updateUserProfile } from "../controller/user.controller.js"
+import { addSkillsToUser, applyForJob, deleteSkillFromUser, getAllApplications, getUserProfile, myProfile, updateProfilePicture, updateResume, updateUserProfile } from "../controller/user.controller.js"
 import uploadFile from "../middleware/multer.middleware.js"
 
 const router = express.Router()
@@ -12,5 +12,7 @@ router.put("/update/pic", isAuth, uploadFile, updateProfilePicture)
 router.put("/update/resume", isAuth, uploadFile, updateResume)
 router.post("/skill/add", isAuth, addSkillsToUser)
 router.delete("/skill/delete", isAuth, deleteSkillFromUser)
+router.post("/apply/job", isAuth, applyForJob)
+router.get("/applications/all", isAuth, getAllApplications)
 
 export default router
